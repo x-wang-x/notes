d522ec58f8ac6ed73f2d90ac726f48bc989bfac2
WanG <69587456+x-wang-x@users.noreply.github.com>
2024-08-04 20:07:59 +0700

    removed unused file and cleanup folder structure

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,8 +1,36 @@
+import './styles/Root.css'
+import Card from './components/Card'
+
 function Root() {
   return (
-    <>
-    ASU
-    </>
+    <div className='container'>
+      <button className='btn'>Add More</button>
+      <textarea className='txt'></textarea>
+      <Card/>
+      <Card/>
+      <Card/>
+      {/* <table className='table'>
+        <thead>
+          <tr>
+              <th>Header 1</th>
+              <th>Header 2</th>
+              <th>Header 3</th>
+          </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Row 1, Cell 1</td>
+                <td>Row 1, Cell 2</td>
+                <td>Row 1, Cell 3</td>
+            </tr>
+            <tr>
+                <td>Row 2, Cell 1</td>
+                <td>Row 2, Cell 2</td>
+                <td>Row 2, Cell 3</td>
+            </tr>
+        </tbody>
+      </table> */}
+    </div>
   )
 }
 
---
+++ src/components/Card.tsx
@@ -0,0 +1,12 @@
+import '../styles/Card.css'
+function Card() {
+  return (
+    <div className='card'>
+        <h1>Title</h1>
+        <p>Content</p>
+        <p>Last update</p>
+    </div>
+  )
+}
+
+export default Card
